b315225b9b4e5a4ce219fa84bab0b74748b3489f
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-08-30 03:50:50 +0300

    Adds memoization.

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,14 +1,17 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getFilter = state => state.contacts.filter;
 
 export const getContacts = state => state.contacts.entities;
 
-export const getFilteredContacts = state => {
-    const contacts = getContacts(state);
-    const filter = getFilter(state);
-    
-    const normalizedFilter = filter.toLowerCase();
-    
-    return contacts.filter(contact =>
-        contact.name.toLowerCase().includes(normalizedFilter),
-    )
-};
+export const getFilteredContacts = createSelector(
+    [getContacts, getFilter],
+    (contacts, filter) => {
+        const normalizedFilter = filter.toLowerCase();
+        
+        return contacts.filter(contact =>
+            contact.name.toLowerCase().includes(normalizedFilter),
+        )
+    }
+);
+
